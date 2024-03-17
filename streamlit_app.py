d94d21bfc040d53ca0773453f36da987615ca826
Sanhitha Cherukupally <sanhitha.c@gmail.com>
2024-03-17 18:57:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,11 @@ import streamlit as st
 import pandas as pd
  
 st.write("""
-# Welcome to Vegellan
+# Welcome to Vegellan!
 """)
+
+option = st.selectbox(
+    'Where are you located?',
+    ('New York', 'California', 'Texas'))
+
+st.write('You selected:', option)

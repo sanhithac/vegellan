1fb7d45f3c5913272a426155cbbd48c13dba0bd3
Sanhitha Cherukupally <sanhitha.c@gmail.com>
2024-03-17 19:00:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
  
 st.write("""
 # Welcome to Vegellan!
@@ -10,3 +11,9 @@ option = st.selectbox(
     ('New York', 'California', 'Texas'))
 
 st.write('You selected:', option)
+
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
